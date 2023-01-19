424d4ea20978fe2dcd11a1aa017065114a6e2427
x <x@x.com>
2023-01-19 13:27:17 +0100

    fix button hover style in dark mode

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -11,7 +11,7 @@ const baseStyles = {
 const variantStyles = {
   solid: {
     slate:
-      'bg-slate-900 dark:bg-slate-100 dark:hover:bg-slate-300 text-white dark:text-gray-900 hover:bg-slate-700 hover:text-slate-100 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900',
+      'bg-slate-900 dark:bg-slate-100 dark:hover:bg-slate-300 text-white dark:text-gray-900 hover:bg-slate-700 hover:text-slate-100 dark:hover:text-slate-700 active:bg-slate-800 active:text-slate-300 focus-visible:outline-slate-900',
     orange: 'bg-orange-600 text-white hover:text-slate-100 hover:bg-orange-500 active:bg-orange-800 active:text-orange-100 focus-visible:outline-orange-600',
     white:
       'bg-white text-slate-900 hover:bg-orange-100 active:bg-orange-200 active:text-slate-600 focus-visible:outline-white',
