67c663580a5e6ce401ab304e812060770d59674f
x <x>
2023-04-22 21:28:11 +0200

    First few stats

--- src/components/dapp/LiquidityBacking.tsx
+++ src/components/dapp/LiquidityBacking.tsx
@@ -1,9 +1,8 @@
 import { ethers } from "ethers"; 
 import { RouteObject } from "react-router-dom"
-import { Button } from "../Button"
-import { useEffect } from "react";
+import { Fragment, useEffect, useState } from "react";
 
-import { getStats } from "../../helpers/liquidityBacking";
+import { getBackingPerDGNX, getStats, getTotalValue } from "../../helpers/liquidityBacking";
 
 const provider = new ethers.JsonRpcProvider('https://avalanche-mainnet-fork.mastertoco.de/', {
     name: 'avalanche',
@@ -11,13 +10,96 @@ const provider = new ethers.JsonRpcProvider('https://avalanche-mainnet-fork.mast
 });
 
 export const LiquidityBacking = (props: RouteObject) => {
+    const [stats, setStats] = useState<Awaited<ReturnType<typeof getStats>>>()
+    const [totalBacking, setTotalBacking] = useState<bigint>()
+    const [loading, setLoading] = useState(false);
+    const [backingPerDGNX, setBackingPerDGNX] = useState<bigint>()
+    const [activeWantToken, setActiveWantToken] = useState<{ decimals: bigint; address: string; info: { name: string }}>()
+
     useEffect(() => {
-        getStats(provider)
+        getStats(provider).then((data) => {
+            setStats(data)
+
+            // Try to set USDC.e as default wantToken, if that isn't in the list, just take the first one
+            const wantTokenIndex = data.wantTokenData.findIndex(token => token.address.toLowerCase() === process.env.NEXT_PUBLIC_USDC_ADDRESS.toLowerCase())
+            setActiveWantToken(data.wantTokenData[wantTokenIndex === -1 ? 0 : wantTokenIndex])
+        })
     }, []);
+
+    useEffect(() => {
+        if (!activeWantToken) {
+            return;
+        }
+        setLoading(true)
+        setTotalBacking(0n)
+        setBackingPerDGNX(0n)
+        Promise.all([
+            getTotalValue(provider, activeWantToken.address).then((data) => {
+                setTotalBacking(data)
+            }),
+            getBackingPerDGNX(provider, activeWantToken.address).then((data) => {
+                setBackingPerDGNX(data)
+            })
+        ]).then(() => setLoading(false))
+    }, [activeWantToken])
 
     return (
         <div>
             <h1 className="text-4xl mb-4">Liquidity Backing</h1>
+            <div className="flex flex-col lg:flex-row w-full">
+                <div className="dark:bg-slate-800 bg-gray-100 p-6 rounded-xl mr-8 mb-8 w-full">
+                    <div className="flex mb-6">
+                        <h3 className="text-xl flex-grow mb-3">Total backing</h3>
+                        {stats?.wantTokenData && (
+                            <select onChange={(e) => setActiveWantToken(stats.wantTokenData.find(wantToken => wantToken.address === e.target.value))} className="dark:bg-slate-900 border dark:border-dark-800 dark:text-slate-200 py-1 leading-3">
+                                {stats.wantTokenData.map(token => {
+                                    return <option key={token.address} value={token.address}>{token.info.name}</option>
+                                })}
+                            </select>
+                            )}
+                    </div>
+                    {!loading && totalBacking > 0 && <div className="text-right text-2xl">
+                        {(Number(totalBacking) / Number(10n ** activeWantToken.decimals)).toFixed(3)} {activeWantToken.info.name}
+                    </div>}
+                    {!loading && backingPerDGNX > 0 && <div className="flex">
+                        <div className="flex-grow"></div>
+                        <div>{(Number(backingPerDGNX) / Number(10n ** activeWantToken.decimals)).toFixed(8)} {activeWantToken.info.name} / DGNX</div>
+                    </div>}
+                </div>
+                <div className="dark:bg-slate-800 bg-gray-100 p-6 rounded-xl mr-8 mb-8 w-full">
+                    <div className="flex mb-6">
+                        <h3 className="text-xl flex-grow mb-3">Your backing</h3>
+                    </div>
+                    <p className="text-center">Connect wallet to see your backing</p>
+                </div>
+                <div className="dark:bg-slate-800 bg-gray-100 p-6 rounded-xl mr-8 mb-8 w-full">
+                    <h3 className="text-xl mb-3">Backing breakdown</h3>
+                    <div className="flex flex-col">
+                        {stats?.vaultData
+                            ? stats?.vaultData.map(vaultItem => {
+                                return (
+                                    <div key={vaultItem.tokenAddress} className="flex">
+                                        <div className="flex-grow">{vaultItem.name}</div>
+                                        <div>{(Number(vaultItem.balance) / Number(10n ** vaultItem.decimals)).toFixed(3)}</div>
+                                    </div>
+                                )
+                            })
+                            : <p>...</p>
+                        }
+                    </div>
+                </div>
+            </div>
+            <h1 className="text-2xl my-4">Burn DGNX for backing</h1>
+            <div className="flex flex-col lg:flex-row w-full">
+                <div className="dark:bg-slate-800 bg-gray-100 p-6 rounded-xl mr-8 mb-8 w-full">
+                    <div className="flex mb-6">
+                        <h3 className="text-xl flex-grow mb-3">Total backing</h3>
+                    </div>
+                    {totalBacking > 0 && <div className="text-right text-2xl">
+                        {(Number(totalBacking) / Number(10n ** activeWantToken.decimals)).toFixed(3)} {activeWantToken.info.name}
+                    </div>}
+                </div>
+            </div>
         </div>
     )
 }
--- src/helpers/liquidityBacking.ts
+++ src/helpers/liquidityBacking.ts
@@ -1,7 +1,30 @@
 import { ethers } from "ethers";
 import { Controller__factory } from "../../types/factories/Controller__factory";
 import { Controller } from "../../types/Controller";
-import { Vault__factory } from "../../types";
+import { Vault__factory } from "../../types/factories/Vault__factory";
+import { Erc20, Erc20__factory } from "../../types";
+
+const getWantTokenData = async (provider: ethers.Provider, controller: Controller, index: number) => {
+    const address = await controller.allWantTokens(index);
+
+    const token = Erc20__factory.connect(address, provider);
+    const [decimals, info] = await Promise.all([
+        token.decimals(),
+        controller.wantTokens(address),
+    ])
+    return { info, address, decimals }
+}
+
+const getWanttokens = async (provider: ethers.Provider, controller: Controller, wantTokenCount: number) => {
+    const indices = Array.from(Array(wantTokenCount).keys());
+    const wantTokenData = (await Promise.all(indices.map(async (index) => {
+        const data = await getWantTokenData(provider, controller, index)
+        console.log(data)
+        return data
+    }))).filter(token => token.info.enabled)
+
+    return wantTokenData;
+}
 
 /**
  * Get token information and balance of every token in a vault
@@ -20,8 +43,8 @@ const getVaultData = async (provider: ethers.Provider, controller: Controller, v
     const amountOfTokensInVault = vault.countAssets;
     const tokensIndices = Array.from(Array(amountOfTokensInVault).keys());
     const tokenInfo = await Promise.all(tokensIndices.map(async (index) => {
-        const tokenAddress = await vault.allAssets(index);
-        const balance = await vault.balanceOf(tokenAddress);
+        const tokenAddress: string = await vault.allAssets(index);
+        const balance: bigint = await vault.balanceOf(tokenAddress);
         return { tokenAddress, balance }
     }))
 
@@ -29,7 +52,8 @@ const getVaultData = async (provider: ethers.Provider, controller: Controller, v
 };
 
 /**
- * Get token information and balance of all vaults
+ * Get token information and balance of all vaults. When vaults have overlapping tokens,
+ * sum them up and return the aggregate
  * 
  * @param provider 
  * @param controller 
@@ -42,14 +66,30 @@ const getVaultsData = async (provider: ethers.Provider, controller: Controller, 
         return getVaultData(provider, controller, index)
     }))).flat().reduce((acc, val) => {
         if (!acc[val.tokenAddress]) {
-            acc[val.tokenAddress] = 0;
+            acc[val.tokenAddress] = 0n;
         }
 
         acc[val.tokenAddress] += val.balance;
         return acc;
-    })
+    }, {} as Record<string, bigint>)
 
-    return vaultData;
+    // Now add the vault name for all tokens
+    const vaultDataIncNames = await Promise.all(Object.entries(vaultData).map(async (tokenInfo) => {
+        const token = Erc20__factory.connect(tokenInfo[0], provider);
+        const [name, decimals] = await Promise.all([
+            token.name(),
+            token.decimals(),
+        ])
+
+        return {
+            tokenAddress: tokenInfo[0],
+            balance: tokenInfo[1],
+            name,
+            decimals,
+        }
+    }))
+
+    return vaultDataIncNames;
 };
 
 /**
@@ -61,9 +101,22 @@ export const getStats = async (provider: ethers.Provider) => {
     const controller = Controller__factory.connect(process.env.NEXT_PUBLIC_CONTROLLER_ADDRESS, provider);
     const counts = await controller.counts();
 
-    const [vaultData] = await Promise.all([
+    const [vaultData, wantTokenData] = await Promise.all([
         getVaultsData(provider, controller, Number(counts.countVaults)),
+        getWanttokens(provider, controller, Number(counts.countWantTokens)),
     ]);
 
-    console.log(vaultData)
+    console.log(wantTokenData)
+
+    return { vaultData, wantTokenData }
+}
+
+export const getTotalValue = async (provider: ethers.Provider, wantToken: string) => {
+    const controller = Controller__factory.connect(process.env.NEXT_PUBLIC_CONTROLLER_ADDRESS, provider);
+    return controller.getTotalValue(wantToken);
+}
+
+export const getBackingPerDGNX = async (provider: ethers.Provider, wantToken: string) => {
+    const controller = Controller__factory.connect(process.env.NEXT_PUBLIC_CONTROLLER_ADDRESS, provider);
+    return controller.getValueOfTokensForOneBaseToken(wantToken);
 }
