23099f840bff5e6776534ce449ecda32870a2fae
x <x>
2023-05-14 08:01:59 +0200

    fix env var

--- api/snowtraceProxy/get.ts
+++ api/snowtraceProxy/get.ts
@@ -1,18 +1,20 @@
-import fetch from "node-fetch"
-import { APIGatewayProxyEvent, APIGatewayProxyResult } from "aws-lambda";
-import { createReturn } from "../helpers/return";
+import fetch from 'node-fetch'
+import { APIGatewayProxyEvent, APIGatewayProxyResult } from 'aws-lambda'
+import { createReturn } from '../helpers/return'
 
 export const handler = async (
-    event: APIGatewayProxyEvent
+  event: APIGatewayProxyEvent
 ): Promise<APIGatewayProxyResult> => {
-    const qs = event.queryStringParameters || {};
+  const qs = event.queryStringParameters || {}
 
-    const resultRaw = await fetch(`https://api.snowtrace.io/api?module=${qs.module}&action=${qs.action}&contractaddress=${qs.contractaddress}&address=${qs.address}&apikey=${process.env.DGNX_API_KEY}`)
-    const result = await resultRaw.json() as any;
-    
-    if (result.message === 'OK') {
-        return createReturn(200, JSON.stringify(result))
-    }
-    
-    return createReturn(403, JSON.stringify(result))
+  const resultRaw = await fetch(
+    `https://api.snowtrace.io/api?module=${qs.module}&action=${qs.action}&contractaddress=${qs.contractaddress}&address=${qs.address}&apikey=${process.env.SNOWTRACE_API_KEY}`
+  )
+  const result = (await resultRaw.json()) as any
+
+  if (result.message === 'OK') {
+    return createReturn(200, JSON.stringify(result))
+  }
+
+  return createReturn(403, JSON.stringify(result))
 }
