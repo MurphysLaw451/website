94bb9d9b38f199a7e53e3c175695fa8757f7a794
Daniel <101750283+TripleD1337@users.noreply.github.com>
2023-05-15 22:12:44 +0200

    Update src/components/dapp/Dashboard.tsx

--- src/components/dapp/Dashboard.tsx
+++ src/components/dapp/Dashboard.tsx
@@ -148,7 +148,7 @@ const getBackingAmount = async () => {
     const resultRaw = await fetch(`${process.env.NEXT_PUBLIC_BACKING_API_ENDPOINT}/getData/avalanche/${process.env.NEXT_PUBLIC_TOKEN_ADDRESS}/${to}/M/1`)
     const result = await resultRaw.json();
     const backingUsd = result.bars?.[0].wantTokens.find(
-        (wantToken) => wantToken.name.toLowerCase().contains('usd')
+        (wantToken) => wantToken.name.toLowerCase().includes('usd')
     )
     return new BigNumber(backingUsd.bOne).div(new BigNumber(10).pow(backingUsd.dec)).toNumber()
 }
