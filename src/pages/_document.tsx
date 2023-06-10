b8bed26e81eaf1c4f5aceef7e854c67eecdc5d53
x <x>
2023-06-10 08:30:17 +0200

    Add gtag

--- src/pages/_document.tsx
+++ src/pages/_document.tsx
@@ -2,40 +2,51 @@ import { Head, Html, Main, NextScript } from 'next/document'
 import { useEffect, useState } from 'react'
 
 export default function Document(props) {
-  let pageProps = props.__NEXT_DATA__?.props?.pageProps
+    let pageProps = props.__NEXT_DATA__?.props?.pageProps
 
-  const [ready, setReady] = useState(false);
+    const [ready, setReady] = useState(false)
 
-  return (
-    <Html
-      className="h-full scroll-smooth antialiased [font-feature-settings:'ss01']"
-      lang="en"
-    >
-      <Head>
-        <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin="anonymous" />
-        <link href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap" rel="stylesheet" />
+    return (
+        <Html
+            className="h-full scroll-smooth antialiased [font-feature-settings:'ss01']"
+            lang="en"
+        >
+            <Head>
+                <link rel="preconnect" href="https://fonts.googleapis.com" />
+                <link
+                    rel="preconnect"
+                    href="https://fonts.gstatic.com"
+                    crossOrigin="anonymous"
+                />
+                <link
+                    href="https://fonts.googleapis.com/css2?family=Space+Mono:wght@400;700&display=swap"
+                    rel="stylesheet"
+                />
 
-        <script
-          async
-          type="text/javascript"
-          src="/charting_library/charting_library.js">
-        </script>
+                <script
+                    async
+                    type="text/javascript"
+                    src="/charting_library/charting_library.js"
+                ></script>
 
-        <script async src="https://www.googletagmanager.com/gtag/js?id=G-SHQ22SS768"></script>
-        <script>
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
+                <script
+                    async
+                    src="https://www.googletagmanager.com/gtag/js?id=G-SHQ22SS768"
+                ></script>
+                <script>
+                    {`window.dataLayer = window.dataLayer || [];
+          function gtag(){
+            dataLayer.push(arguments);
+          }
           gtag('js', new Date());
 
-          gtag('config', 'G-SHQ22SS768');
-        </script>
-
-      </Head>
-      <body className="flex h-full flex-col bg-white dark:bg-gray-900 ">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  )
+          gtag('config', 'G-SHQ22SS768');`}
+                </script>
+            </Head>
+            <body className="flex h-full flex-col bg-white dark:bg-gray-900 ">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    )
 }
