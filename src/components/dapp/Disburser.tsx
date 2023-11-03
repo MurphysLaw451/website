08fa7c4076aace50b583ef553f62632f3fa71549
Daniel <danieldegendev@gmail.com>
2023-11-03 16:33:23 +0100

    updated legacy disburser

--- src/components/dapp/Disburser.tsx
+++ src/components/dapp/Disburser.tsx
@@ -11,7 +11,7 @@ export const Disburser = () => {
                 <span className="text-techGreen">LEGACY</span>
                 <span className="text-degenOrange">DISBURSER</span>
             </h1>
-            <div className="rounded-xl border-2 border-degenOrange bg-light-100 p-5 text-light-200 dark:border-activeblue dark:bg-darkerblue  dark:text-light-200">
+            <div className="rounded-xl border-2 border-degenOrange bg-light-100 p-5 text-light-200 dark:border-activeblue dark:bg-darkerblue  dark:text-light-200  mb-4">
                 This page is meant for Degens that held the old Degen SD tokens. For more information,
                 visit our docs <a className="text-degenOrange" href="https://docs.dgnx.finance/ecosystem/products/disburser" target="_blank" rel="noreferrer">here</a>.
 
--- src/components/dapp/elements/DisburserApp.tsx
+++ src/components/dapp/elements/DisburserApp.tsx
@@ -1,47 +1,50 @@
 import { RouteObject } from 'react-router-dom'
-import { useAccount, useContractRead, useContractWrite } from 'wagmi'
-import abi from '../../../abi/disburser.json';
-import { Spinner } from './Spinner';
-import BigNumber from 'bignumber.js';
-import { useCountdownTimer } from 'use-countdown-timer';
-import { useEffect } from 'react';
-import { Button } from '../../Button';
+import { useAccount, useContractRead, useContractWrite, useClient } from 'wagmi'
+import abi from '../../../abi/disburser.json'
+import { Spinner } from './Spinner'
+import BigNumber from 'bignumber.js'
+import { useCountdownTimer } from 'use-countdown-timer'
+import { useEffect } from 'react'
+import { Button } from '../../Button'
 
-const DISBURSER_ADDRESS = '0x8a0E3264Da08bf999AfF5a50AabF5d2dc89fab79';
+const DISBURSER_ADDRESS = '0x8a0E3264Da08bf999AfF5a50AabF5d2dc89fab79'
 
 const countdownStr = (s: number) => {
-    const d = Math.floor(s / (3600 * 24));
-    s  -= d * 3600 * 24;
-    const h = Math.floor(s / 3600);
-    s  -= h * 3600;
-    const m = Math.floor(s / 60);
-    s  -= m * 60;
-    const tmp = [];
-    (d) && tmp.push(d + 'd');
-    (d || h) && tmp.push(h + 'h');
-    (d || h || m) && tmp.push(m + 'm');
-    tmp.push(s + 's');
-    return tmp.join(' ');
+    const d = Math.floor(s / (3600 * 24))
+    s -= d * 3600 * 24
+    const h = Math.floor(s / 3600)
+    s -= h * 3600
+    const m = Math.floor(s / 60)
+    s -= m * 60
+    const tmp = []
+    d && tmp.push(d + 'd')
+    ;(d || h) && tmp.push(h + 'h')
+    ;(d || h || m) && tmp.push(m + 'm')
+    tmp.push(s + 's')
+    return tmp.join(' ')
 }
 
 const Countdown = (props: { seconds: number }) => {
     const { countdown, start, isRunning } = useCountdownTimer({
-        timer: 1000 * props.seconds
-    });
+        timer: 1000 * props.seconds,
+    })
 
     useEffect(() => {
         if (!isRunning) {
-            start();
+            start()
         }
-    }, [isRunning]);
+    }, [isRunning])
 
-    return (
-        <div>Claim in {countdownStr(countdown / 1000)}</div>
-    )
+    return <div>Claim in {countdownStr(countdown / 1000)}</div>
 }
 
-const ClaimButton = () => {
-    const { data, isLoading: claimLoading, isSuccess: claimSuccess, write: claim } = useContractWrite({
+const ClaimButton = (props: { amount: string | number }) => {
+    const {
+        data,
+        isLoading: claimLoading,
+        isSuccess: claimSuccess,
+        write: claim,
+    } = useContractWrite({
         address: DISBURSER_ADDRESS,
         abi,
         functionName: 'claim',
@@ -49,34 +52,31 @@ const ClaimButton = () => {
     })
 
     if (claimLoading) {
-        return <Button
-            className="w-full mt-3"
-            color="orangeDisabled"
-        >
-            Loading...
-        </Button>
+        return (
+            <Button className="mt-3 w-full" color="orangeDisabled">
+                Loading...
+            </Button>
+        )
     }
 
     if (claimSuccess) {
-        return <Button
-            className="w-full mt-3"
-            color="orangeDisabled"
-        >
-            Claim success!
-        </Button>
+        return (
+            <Button className="mt-3 w-full" color="orangeDisabled">
+                Claim success!
+            </Button>
+        )
     }
 
-    return <Button
-        className="w-full mt-3"
-        color="orange"
-        onClick={() => claim()}
-    >
-        Claim now!
-    </Button>
+    return (
+        <Button className="mt-3 w-full" color="orange" onClick={() => claim()}>
+            Claim {props.amount} now!
+        </Button>
+    )
 }
 
 const Dapp = () => {
     const { address } = useAccount()
+    const { data: walletClient } = useClient()
     const { data: amountLeft, isLoading: amountLeftLoading } = useContractRead({
         address: DISBURSER_ADDRESS,
         abi,
@@ -91,38 +91,62 @@ const Dapp = () => {
         args: [address],
     })
 
-    const { data: timeUntilNextClaimBN } = useContractRead({
+    const {
+        data: timeUntilNextClaimBN,
+        isLoading: timeUntilNextClaimBNLoading,
+    } = useContractRead({
         address: DISBURSER_ADDRESS,
         abi,
         functionName: 'timeLeftUntilNextClaim',
         args: [address],
     })
 
-    const timeUntilNextClaim = ((timeUntilNextClaimBN) as BigNumber).toNumber();
-    const amountClaimedStr = amountLeftLoading ? '...' : `${(parseInt(amountLeft as string) / Math.pow(10, 18)).toFixed(4)}`;
-    const amountLeftStr = paidOutLoading ? '...' : `${(parseInt(paidOut as string) / Math.pow(10, 18)).toFixed(4)}`;
+    const { data: claimEstimate, isLoading: claimEstimateLoading } =
+        useContractRead({
+            address: DISBURSER_ADDRESS,
+            abi,
+            functionName: 'claimEstimate',
+            overrides: {
+                from: walletClient.account,
+            },
+        })
+
+    const claimableAmount = claimEstimateLoading
+        ? 0
+        : `${(
+              parseInt((claimEstimate as any).claimable as string) /
+              Math.pow(10, 18)
+          ).toFixed(4)}`
+    const timeUntilNextClaim = timeUntilNextClaimBNLoading
+        ? 0
+        : (timeUntilNextClaimBN as BigNumber).toNumber()
+    const amountLeftStr = amountLeftLoading
+        ? '...'
+        : `${(parseInt(amountLeft as string) / Math.pow(10, 18)).toFixed(4)}`
+    const amountClaimedStr = paidOutLoading
+        ? '...'
+        : `${(parseInt(paidOut as string) / Math.pow(10, 18)).toFixed(4)}`
 
     return (
         <>
-            <div className="grid grid-cols-1 sm:grid-cols-2 gap-6">
-                <div className="flex flex-col items-center justify-center border-2 border-activeblue rounded-xl p-3 bg-dark">
-                    <h2 className="font-bold text-2xl">Claimed</h2>
+            <div className="grid grid-cols-1 gap-6 sm:grid-cols-2">
+                <div className="flex flex-col items-center justify-center rounded-xl border-2 border-activeblue bg-dark p-3">
+                    <h2 className="text-2xl font-bold">Claimed</h2>
                     <div className="mt-3 text-xl">{amountClaimedStr} DGNX</div>
                 </div>
-                <div className="flex flex-col items-center justify-center border-2 border-activeblue rounded-xl p-3 bg-dark">
-                    <h2 className="font-bold text-2xl">Left to claim</h2>
+                <div className="flex flex-col items-center justify-center rounded-xl border-2 border-activeblue bg-dark p-3">
+                    <h2 className="text-2xl font-bold">Left to claim</h2>
                     <div className="mt-3 text-xl">{amountLeftStr} DGNX</div>
                 </div>
             </div>
-            <div className="mt-6 flex justify-center items-center w-full">
-                {timeUntilNextClaim > 0 
-                    ? <Button
-                        className="w-full mt-3"
-                        color="orangeDisabled"
-                    >
+            <div className="mt-6 flex w-full items-center justify-center">
+                {timeUntilNextClaim > 0 ? (
+                    <Button className="mt-3 w-full" color="orangeDisabled">
                         <Countdown seconds={timeUntilNextClaim} />
                     </Button>
-                    : <ClaimButton />}
+                ) : (
+                    <ClaimButton amount={claimableAmount} />
+                )}
             </div>
         </>
     )
@@ -138,16 +162,25 @@ export const DisburserApp = (props: RouteObject) => {
     })
 
     if (!isConnected) {
-        return <div className="font-bold">Please connect wallet</div>;
+        return <div className="font-bold">Please connect wallet</div>
     }
-    
+
     if (isLoading) {
-        return <div className="w-full flex items-center justify-center"><Spinner /></div>;
+        return (
+            <div className="flex w-full items-center justify-center">
+                <Spinner />
+            </div>
+        )
     }
 
     if (hasAmountLeft) {
         return <Dapp />
     }
 
-    return <div>Unfortunately you don&apos;t have a claim on the legacy disburser (anymore).</div>
+    return (
+        <div>
+            Unfortunately you don&apos;t have a claim on the legacy disburser
+            (anymore).
+        </div>
+    )
 }
