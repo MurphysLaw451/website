f8f8196e4b4c7efe25561d966b89476fc1bf7d3a
Daniel <danieldegendev@gmail.com>
2024-01-12 16:39:55 +0100

    Disable sentry

--- next.config.js
+++ next.config.js
@@ -1,5 +1,5 @@
 /** @type {import('next').NextConfig} */
-const { withSentryConfig } = require('@sentry/nextjs')
+// const { withSentryConfig } = require('@sentry/nextjs')
 
 const nextConfig = {
   async rewrites() {
@@ -20,22 +20,23 @@ const nextConfig = {
   },
 }
 
-const sentryWebpackPluginOptions = {
-  // Additional config options for the Sentry webpack plugin. Keep in mind that
-  // the following options are set automatically, and overriding them is not
-  // recommended:
-  //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
+// const sentryWebpackPluginOptions = {
+//   // Additional config options for the Sentry webpack plugin. Keep in mind that
+//   // the following options are set automatically, and overriding them is not
+//   // recommended:
+//   //   release, url, configFile, stripPrefix, urlPrefix, include, ignore
 
-  org: 'degenx-ecosystem',
-  project: 'degenx-dapp-staging',
+//   org: 'degenx-ecosystem',
+//   project: 'degenx-dapp-staging',
 
-  // An auth token is required for uploading source maps.
-  authToken: process.env.SENTRY_AUTH_TOKEN,
+//   // An auth token is required for uploading source maps.
+//   authToken: process.env.SENTRY_AUTH_TOKEN,
 
-  silent: true, // Suppresses all logs
+//   silent: true, // Suppresses all logs
 
-  // For all available options, see:
-  // https://github.com/getsentry/sentry-webpack-plugin#options.
-}
+//   // For all available options, see:
+//   // https://github.com/getsentry/sentry-webpack-plugin#options.
+// }
 
-module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+// module.exports = withSentryConfig(nextConfig, sentryWebpackPluginOptions)
+module.exports = nextConfig
